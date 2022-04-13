b481c5be7644fac2094a5c59590f77b6325d38d6
Sam Sharff <samanthaleec@gmail.com>
2022-04-13 11:49:23 -0400

    adds index action to HomeView.vue; successfully tests in browser

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to a list of places!",
+      places: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("/places").then((response) => {
+        console.log("All places", response);
+        this.places = response.data;
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="place in places" v-bind:key="place.id">
+      <h2>Name: {{ place.name }}</h2>
+      <h4>Address: {{ place.address }}</h4>
+    </div>
   </div>
 </template>
 
